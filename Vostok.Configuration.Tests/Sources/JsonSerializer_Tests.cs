10a0202ff262635fb269b00978ff0198acb8cd1b
razboynikov <razboynikov@skbkontur.ru>
2018-03-26 12:07:47 +0500

    JsonSerializer added.

---
+++ Vostok.Configuration.Tests/Sources/JsonSerializer_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Configuration.Sources;
+
+namespace Vostok.Configuration.Tests.Sources
+{
+    [TestFixture]
+    public class JsonSerializer_Tests
+    {
+        private const string TestFileName = "test_JsonSerializer.json";
+
+        [TearDown]
+        public void Cleanup()
+        {
+            File.Delete(TestFileName);
+        }
+
+        [Test]
+        public void Should_serialize_json()
+        {
+            var obj = new MyClass
+            {
+                IntValue = 123,
+                StringValue = "qwe",
+                InnerClass = new MyClass
+                {
+                    IntValue = 321,
+                    StringValue = "ewq",
+                }
+            };
+            JsonSerializer.Serialize(obj, TestFileName, SerializeOption.Short);
+            File.ReadAllText(TestFileName).Split(new[] {'\r', '\n'}, StringSplitOptions.RemoveEmptyEntries).Should().HaveCount(1);
+
+            JsonSerializer.Serialize(obj, TestFileName, SerializeOption.Readable);
+            File.ReadAllText(TestFileName).Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Should().HaveCountGreaterThan(3);
+        }
+
+        private class MyClass
+        {
+            public int IntValue { get; set; }
+            public string StringValue { get; set; }
+            public MyClass InnerClass { get; set; }
+        }
+    }
+}
---
+++ Vostok.Configuration/Sources/JsonSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace Vostok.Configuration.Sources
+{
+    public enum SerializeOption
+    {
+        Short,
+        Readable,
+    }
+
+    public class JsonSerializer
+    {
+        public static void Serialize(object obj, string filePath, SerializeOption serializeOption = SerializeOption.Short)
+        {
+            var serializer = new Newtonsoft.Json.JsonSerializer
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = serializeOption == SerializeOption.Short ? Formatting.None : Formatting.Indented,
+            };
+            using (var sw = new StreamWriter(filePath))
+                using (JsonWriter writer = new JsonTextWriter(sw))
+                    serializer.Serialize(writer, obj);
+        }
+    }
+}
