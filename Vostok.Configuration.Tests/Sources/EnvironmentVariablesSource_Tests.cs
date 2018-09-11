391c7fd155b1ccea2e481c97673f8e47c1c6dc7a
razboynikov <razboynikov@skbkontur.ru>
2018-09-11 15:31:29 +0500

    Fixed environment variables tests for Ubuntu.

--- Vostok.Configuration.Tests/Sources/EnvironmentVariablesSource_Tests.cs
+++ Vostok.Configuration.Tests/Sources/EnvironmentVariablesSource_Tests.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using FluentAssertions;
 using NUnit.Framework;
 using Vostok.Commons.Helpers.Conversions;
 using Vostok.Commons.Testing;
+using Vostok.Configuration.Abstractions.SettingsTree;
 using Vostok.Configuration.Sources;
 
 namespace Vostok.Configuration.Tests.Sources
@@ -16,8 +18,8 @@ namespace Vostok.Configuration.Tests.Sources
         {
             var evs = new EnvironmentVariablesSource();
             var res = evs.Get();
-            res["pAtH"].Value.Should().NotBeNull();
-            res["APPdata"].Value.Should().NotBeNull();
+
+            CheckResult(res);
         }
 
         [Test]
@@ -25,19 +27,35 @@ namespace Vostok.Configuration.Tests.Sources
         {
             new Action(() => ShouldObserveVariablesTest_ReturnsIfValueWasReceived().Should().BeTrue()).ShouldPassIn(1.Seconds());
         }
-        private bool ShouldObserveVariablesTest_ReturnsIfValueWasReceived()
+        private static bool ShouldObserveVariablesTest_ReturnsIfValueWasReceived()
         {
             var val = false;
             var evs = new EnvironmentVariablesSource();
             var sub = evs.Observe().Subscribe(settings =>
             {
                 val = true;
-                settings["Path"].Value.Should().NotBeNull();
-                settings["APPDATA"].Value.Should().NotBeNull();
+                CheckResult(settings);
             });
             Thread.Sleep(200.Milliseconds());
             sub.Dispose();
             return val;
+        }
+
+        private static void CheckResult(ISettingsNode settings)
+        {
+            var windows = new[] { PlatformID.Win32NT, PlatformID.Win32S, PlatformID.Win32Windows, PlatformID.WinCE };
+            if (windows.Contains(Environment.OSVersion.Platform))
+            {
+                settings["pAtH"].Value.Should().NotBeNull();
+                settings["APPdata"].Value.Should().NotBeNull();
+            }
+            else if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                settings["pAtH"].Value.Should().NotBeNull();
+                settings["sheLL"].Value.Should().NotBeNull();
+            }
+            else
+                throw new NotImplementedException();
         }
     }
 }
