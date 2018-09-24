9ad0f4a8c2ab29309430ab44a540e58e0751ab97
Krait <krait@skbkontur.ru>
2018-09-24 13:03:32 +0500

    Minor fixes.

--- Vostok.Configuration/Binders/ISettingsBinderFactory.cs
+++ Vostok.Configuration/Binders/ISettingsBinderFactory.cs
@@ -6,6 +6,7 @@ namespace Vostok.Configuration.Binders
     internal interface ISettingsBinderFactory
     {
         ISettingsBinder<T> CreateFor<T>();
+
         ISettingsBinder<object> CreateFor(Type type);
     }
 }
