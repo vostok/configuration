10603e4b2098f01bec90899ea2bf6919a5d75bf4
Krait <krait@skbkontur.ru>
2019-02-14 11:24:23 +0500

    ValidatingBinder: removed unnecessary 'prefix' parameter.

--- Vostok.Configuration/Binders/ValidatingBinder.cs
+++ Vostok.Configuration/Binders/ValidatingBinder.cs
@@ -28,14 +28,14 @@ namespace Vostok.Configuration.Binders
         {
             var type = typeof(TSettings);
 
-            var errors = Validate(type, value, "").ToList();
+            var errors = Validate(type, value).ToList();
             if (errors.Any())
             {
                 throw new SettingsValidationException(string.Join(Environment.NewLine, errors));
             }
         }
 
-        private static IEnumerable<string> Validate(Type type, object value, string prefix)
+        private static IEnumerable<string> Validate(Type type, object value)
         {
             if (value == null)
                 yield break;
@@ -47,21 +47,21 @@ namespace Vostok.Configuration.Binders
             if (validateMethod == null)
                 throw new SettingsValidationException($"Type '{validator.GetType()}' specified as validator for settings of type '{type}' does not contain a suitable {nameof(ISettingsValidator<object>.Validate)} method.");
             foreach (var error in (IEnumerable<string>)validateMethod.Invoke(validator, new[] {value}))
-                yield return FormatError(prefix, error);
+                yield return error;
 
             foreach (var field in type.GetInstanceFields())
             {
-                foreach (var error in Validate(field.FieldType, field.GetValue(value), field.Name))
-                    yield return FormatError(prefix, error);
+                foreach (var error in Validate(field.FieldType, field.GetValue(value)))
+                    yield return FormatError(field.Name, error);
             }
 
             foreach (var prop in type.GetInstanceProperties())
             {
-                foreach (var error in Validate(prop.PropertyType, prop.GetValue(value), prop.Name))
-                    yield return FormatError(prefix, error);
+                foreach (var error in Validate(prop.PropertyType, prop.GetValue(value)))
+                    yield return FormatError(prop.Name, error);
             }
         }
 
-        private static string FormatError(string prefix, string error) => prefix == "" ? error : prefix + "." + error;
+        private static string FormatError(string prefix, string error) => prefix + "." + error;
     }
 }
