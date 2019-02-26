4d274f6ff07bd1a40b6082240f4d4ec47bed3b61
iloktionov <iloktionov@skbkontur.ru>
2019-02-26 18:06:34 +0500

    HealingObservable: reformat.

--- Vostok.Configuration/Helpers/HealingObservable.cs
+++ Vostok.Configuration/Helpers/HealingObservable.cs
@@ -7,7 +7,14 @@ namespace Vostok.Configuration.Helpers
     {
         public static IObservable<(T, Exception)> PushAndResubscribeOnErrors<T>(Func<IObservable<(T, Exception)>> observe, TimeSpan cooldown)
         {
-            return Observable.Defer(observe).Catch<(T, Exception), Exception>(e => Observable.Throw<(T, Exception)>(e).DelaySubscription(cooldown).StartWith((default, e))).Retry();
+            return Observable
+                .Defer(observe)
+                .Catch<(T, Exception), Exception>(
+                    e => Observable
+                        .Throw<(T, Exception)>(e)
+                        .DelaySubscription(cooldown)
+                        .StartWith((default, e)))
+                .Retry();
         }
     }
 }
