e153c65271e6a2af2cd0680aee8db39f9c59e2cb
iloktionov <iloktionov@skbkontur.ru>
2019-03-01 13:08:05 +0500

    SettingsBindingResult: do not include exception stack traces as error messages.

--- Vostok.Configuration/Binders/Results/SettingsBindingResult.cs
+++ Vostok.Configuration/Binders/Results/SettingsBindingResult.cs
@@ -7,7 +7,7 @@ namespace Vostok.Configuration.Binders.Results
     internal static class SettingsBindingResult
     {
         public static SettingsBindingResult<TSettings> NodeTypeMismatch<TSettings>(ISettingsNode node) =>
-            Error<TSettings>($"A {node.GetType().Name} cannot be bound to '{typeof(TSettings)}'");
+            Error<TSettings>($"A(n) {node.GetType().Name} cannot be bound to '{typeof(TSettings)}'");
 
         public static SettingsBindingResult<TSettings> ParsingError<TSettings>(string value) =>
             Error<TSettings>($"Value '{value}' cannot be parsed as '{typeof(TSettings)}'.");
@@ -38,7 +38,7 @@ namespace Vostok.Configuration.Binders.Results
             }
             catch (Exception error)
             {
-                return Error<TSettings>(error.ToString());
+                return Error<TSettings>($"{error.GetType().FullName}: {error.Message}");
             }
         }
     }
