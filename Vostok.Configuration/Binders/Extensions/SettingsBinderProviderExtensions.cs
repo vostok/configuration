c3ae356117a9b814b647b62059cf1e8dd3c79008
kungurtsev <kungurtsev@skbkontur.ru>
2020-11-03 12:48:25 +0500

    base64 -> byte[]

--- Vostok.Configuration/Binders/Extensions/SettingsBinderProviderExtensions.cs
+++ Vostok.Configuration/Binders/Extensions/SettingsBinderProviderExtensions.cs
@@ -24,6 +24,7 @@ namespace Vostok.Configuration.Binders.Extensions
                 .WithParserFor<int>(int.TryParse)
                 .WithParserFor<long>(long.TryParse)
                 .WithParserFor<byte>(byte.TryParse)
+                .WithParserFor<byte[]>(BytesArrayParser.TryParse)
                 .WithParserFor<ushort>(ushort.TryParse)
                 .WithParserFor<uint>(uint.TryParse)
                 .WithParserFor<ulong>(ulong.TryParse)
---
+++ Vostok.Configuration/Parsers/ByteArrayParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Parsers
+{
+    [PublicAPI]
+    public static class ByteArrayParser
+    {
+        public static bool TryParse(string input, out byte[] result)
+        {
+            try
+            {
+                result = Convert.FromBase64String(input);
+                return true;
+            }
+            catch
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+}
