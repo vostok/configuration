d84ff4a24b346eb19c345ca855e81db36fd99c30
kungurtsev <kungurtsev@skbkontur.ru>
2021-12-17 20:26:49 +0500

    return ParseMethodFinder

---
+++ Vostok.Configuration.Tests/Helpers/ParseMethodFinder_Tests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Configuration.Helpers;
+
+namespace Vostok.Configuration.Tests.Helpers
+{
+    [TestFixture]
+    internal class ParseMethodFinder_Tests
+    {
+        [Test]
+        public void Should_detect_parse_method()
+        {
+            ParseMethodFinder.FindParseMethod(typeof(int)).Should().NotBeNull();
+            ParseMethodFinder.FindParseMethod(typeof(Guid)).Should().NotBeNull();
+            ParseMethodFinder.FindParseMethod(typeof(IPAddress)).Should().NotBeNull();
+        }
+
+        [Test]
+        public void Should_detect_tryparse_method()
+        {
+            ParseMethodFinder.FindTryParseMethod(typeof(int)).Should().NotBeNull();
+            ParseMethodFinder.FindTryParseMethod(typeof(Guid)).Should().NotBeNull();
+            ParseMethodFinder.FindTryParseMethod(typeof(IPAddress)).Should().NotBeNull();
+        }
+
+        [Test]
+        public void Should_not_detect_methods_where_there_are_none()
+        {
+            ParseMethodFinder.FindParseMethod(GetType()).Should().BeNull();
+
+            ParseMethodFinder.FindTryParseMethod(GetType()).Should().BeNull();
+        }
+    }
+}
