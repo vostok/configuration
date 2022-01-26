e4c663eb555b8422d4af9edd47b62bebce19f3b4
Ilya Tzoop <ilyatzoop@gmail.com>
2022-01-26 14:16:22 +0500

    Added CustomFormattersExtended

---
+++ Vostok.Configuration/Helpers/CustomFormattersExtended.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Vostok.Commons.Formatting;
+using Vostok.Configuration.Abstractions.SettingsTree;
+
+namespace Vostok.Configuration.Helpers
+{
+    internal static class CustomFormattersExtended
+    {
+        private static readonly Dictionary<Type, Func<object, string>> Formatters
+            = new()
+            {
+                [typeof(ISettingsNode)] = value => value.ToString()
+            };
+
+        public static bool TryFormat(object item, out string s) =>
+            CustomFormatters.TryFormatWithExplicitFormatters(item, Formatters, out s) || CustomFormatters.TryFormat(item, out s);
+    }
+}
